78809691c1bb831630808bcfc14562e2b9272fd2
chenchunyang123 <ccy274002525@163.com>
2019-12-22 00:59:24 +0800

    初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    title: '一条放了盐的鱼',
+    description: '前端笔记|前端博客|静态博客网站',
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            {
+                text: '我的笔记',
+                items: [
+                    { text: 'React', link: '/react/开始' },
+                    // { text: '微信小程序', link: '/miniprogram/' },
+                    // { text: 'Vue', link: '/vue/' },
+                    // { text: 'Webpack', link: '/webpack/' },
+                    // { text: 'Node', link: '/node/' },
+                ]
+            },
+            { text: '我的博客', link: 'http://chenchunyang.site/' },
+            { text: 'Github', link: 'https://github.com/chenchunyang123' },
+        ],
+        sidebar: {
+            '/react/': [
+                '开始',
+                'aa'
+            ],
+        }
+    },
+    base: '/',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': 'public'
+            }
+        }
+    },
+    plugins: [
+        '@vuepress/back-to-top',
+        'vuepress-plugin-smooth-scroll'
+    ]
+}
